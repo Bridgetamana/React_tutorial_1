8b4172e97d4e052b21c1e33d6f51aa53282f95a7
Bridget Amana <amanabridget03@gmail.com>
2024-03-19 22:46:21 +0100

    list rendering lesson

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import ClassClick from './components/ClassClick';
 import Click from './components/Click';
 import Counter from './components/Counter';
 import Greet from './components/Greet';
+import NameList from './components/NameList';
 
 function App() {
   return (
     <div>
       <Click />
       <ClassClick />
+      <NameList />
       {/* <Greet name='Bridget'/>
       <Counter /> */}
     </div>
---
+++ src/components/NameList.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const NameList = () => {
+    const persons = [
+        {
+            id: 1,
+            name: 'Alice',
+            age: 30,
+            skill: 'React',
+        }, 
+        {
+            id: 2,
+            name: 'Mary',
+            age: 37,
+            skill: 'Angular',
+        },
+        {
+            id: 3,
+            name: 'John',
+            age: 22,
+            skill: 'Javascript',
+        },
+        {
+            id: 4,
+            name: 'Smith',
+            age: 25,
+            skill: 'Vue',
+        }
+    ]
+    // const personList = persons.map(person => <person key={person.id} person={person} />)
+    
+    return (
+    <div className="text-center py-12">
+        {
+            persons.map(person => (
+                <h2>I am {person.name}, I am {person.age} years old and I know {person.skill}</h2>
+            ))
+        }
+    </div>
+  )
+}
+
+export default NameList
