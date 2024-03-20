920f795cc600ac52a6762e659116cef2d0bf9cd1
Bridget Amana <amanabridget03@gmail.com>
2024-03-20 22:20:14 +0100

    Conditional Rendering lesson

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,6 +4,7 @@ class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = { time: new Date().toLocaleTimeString() };
+    this.toggleVisibility = this.toggleVisibility.bind(this);
   }
 
   componentDidMount() {
@@ -16,11 +17,29 @@ class Clock extends Component {
     clearInterval(this.timerID);
   }
 
+  toggleVisibility() {
+    this.setState((prevState) => ({
+      isVisible: !prevState.isVisible,
+    }));
+
+}
+
   render() {
     return (
-      <div className='flex justify-center py-16'>
-        <h2 className='mr-2 font-semibold'>Current Time:</h2>
-        <p>{this.state.time}</p>
+        <div className='flex flex-row-reverse justify-center py-16 items-center gap-4'>
+        <button className='py-2 px-6 bg-black text-white rounded-md' onClick={this.toggleVisibility}>
+          {this.state.isVisible ? 'Hide Clock' : 'Show Clock '}
+        </button>
+        {this.state.isVisible ? (
+          <div>
+            <h2 className='mr-2 font-semibold'>Current Time:</h2>
+            <p>{this.state.time}</p>
+          </div>
+        ) : (
+          <div>
+            <h2>The clock is hidden</h2>
+          </div>
+        )}
       </div>
     );
   }
